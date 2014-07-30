b556553b1c2c2dd7cd302bb5fa7b4b35eacbaba9
Herbert Shi <351096578@qq.com>
2014-07-31 00:33:08 +0800

    初步实现

--- js/config.js
+++ js/config.js
@@ -2,8 +2,32 @@ define(function() {
 	var config = {
 		page:[{
 			name:"1",
-			url:"1.html"
-		}]
+			url:"1.html",
+            beforeLoad:function(){
+                
+            },
+            afterLoad:function(){
+
+            }
+		},{
+            name:"2",
+            url:"2.html",
+            beforeLoad:function(){
+                
+            },
+            afterLoad:function(){
+                
+            }
+        },{
+            name:"3",
+            url:"3.html",
+            beforeLoad:function(){
+                
+            },
+            afterLoad:function(){
+                
+            }
+        }]
 	};
 	return config;
 });
--- js/index.js
+++ js/index.js
@@ -3,12 +3,109 @@ requirejs.config({
     paths: {
         'jquery': 'jquery-1.11.1.min',
         'css': 'css.min',
+        'swipe': 'jquery.touchSwipe.min',
         'config': 'config'
+    },
+    shim: {
+        'swipe': {
+            deps: ['jquery']
+        }
     }
 });
 
-requirejs(['jquery', 'config'], function($, Config) {
+requirejs(['jquery', 'config', 'swipe'], function($, Config) {
+    var currentIndex = 0;
     $(function() {
+        $(Config.page).map(function(i) {
+            $("<div id='" + this.name + "' data-role='page' data-index='" + i + "'></div>").css({
+                "position": "absolute",
+                "width": "100%",
+                "height": "100%",
+                "left": "0%",
+                "top": (100 * i) + "%"
+            }).data(this).swipe({
+                swipe: function(event, direction, distance, duration, fingerCount) {
+                    loadPage(direction);
+                }
+            }).appendTo($("body"));
+        });
 
+        loadPage("first");
     });
+
+    function loadPage(direction) {
+        if (direction == "up") {
+            currentIndex++;
+        } else if (direction == "down") {
+            currentIndex--;
+        }
+
+        var newObj = $("div[data-role='page'][data-index='"+currentIndex+"']"),
+            data = newObj.data(),
+            oldObj,goAnimate,comeAnimate;
+
+        if (direction == "up") {
+            oldObj = $("div[data-role='page'][data-index='"+(currentIndex-1)+"']");
+            if(newObj.size()>0 && newObj.html()){
+                goAnimate = function(){
+                    oldObj.animate({"top": "-105%"}, 1000, function() {
+                        $(this).animate({"top": "-100%" }, 500);
+                    });
+                }
+
+                comeAnimate = function() {
+                    newObj.animate({"top": "-5%"}, 1000, function() {
+                        $(this).animate({"top": "0%" }, 500, function() {
+                            data.afterLoad();
+                        });
+                    });
+                }
+            }else{
+                oldObj.animate({"top": "-5%"}, 500, function() {
+                    $(this).animate({"top": "0%" }, 400);
+                });
+                currentIndex--
+            }
+
+        } else if (direction == "down") {
+            oldObj = $("div[data-role='page'][data-index='"+(currentIndex+1)+"']");
+
+            if(newObj.size()>0 && newObj.html()){
+                goAnimate = function(){
+                    oldObj.animate({"top": "105%"}, 1000, function() {
+                        $(this).animate({"top": "100%"}, 500);
+                    });
+                }
+
+                comeAnimate = function() {
+                    newObj.animate({"top": "5%"}, 1000, function() {
+                        $(this).animate({"top": "0%"}, 500, function() {
+                            data.afterLoad();
+                        });
+                    });
+                }
+            }else{
+                oldObj.animate({"top": "5%"}, 500, function() {
+                    $(this).animate({"top": "0%" }, 400);
+                });
+                currentIndex++;
+            }
+        }else if(direction == "first"){
+            newObj.load(data.url,function(){
+                data.afterLoad();
+            });
+        }
+
+        if (goAnimate && comeAnimate) {
+            data.beforeLoad();
+            comeAnimate();
+            goAnimate();
+        }
+
+
+        var nextObj = newObj.next("div[data-role='page']");
+        if(nextObj.html() == ""){
+            nextObj.load(nextObj.data("url"));
+        }
+    }
 });
