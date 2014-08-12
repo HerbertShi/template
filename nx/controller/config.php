e40e8a8e714a549de6fcc92b474b1a5aff6e42c3
Herbert Shi <351096578@qq.com>
2014-08-13 01:01:09 +0800

    add admin.html

--- nx/controller/config.php
+++ nx/controller/config.php
@@ -1,6 +1,9 @@
 <?php
-	$db_host = '192.168.0.91';
+	//$db_host = '192.168.0.91';
+    $db_host = 'localhost';
 	$db_name = 'test';
-	$db_users = 'jq';
-	$db_pass = 'jq';
+	// $db_users = 'jq';
+	// $db_pass = 'jq';
+    $db_users = 'root';
+    $db_pass = '1234';
 ?>
--- nx/controller/storySubmit.php
+++ nx/controller/storySubmit.php
@@ -14,6 +14,6 @@
 	"','".$phone.
 	"' ) ";
 	$res = mysql_query($sql,$con); 
-	
+	mysql_close($con);
 	echo $res;
 ?>
