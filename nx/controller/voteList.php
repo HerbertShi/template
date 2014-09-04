4ccfb99074dd89457e408ba638743ca51dc5de06
Herbert Shi <351096578@qq.com>
2014-09-04 18:46:33 +0800

    add style

--- nx/controller/voteList.php
+++ nx/controller/voteList.php
@@ -4,7 +4,7 @@
     $con = mysql_connect($db_host,$db_users,$db_pass);
     mysql_select_db($db_name, $con);
     mysql_query("SET NAMES UTF8");
-    $sql = "select * from nx_ad group by concat(advertising,meaning,name,phone) ORDER BY createDate DESC ";
+    $sql = "select * from nx_ad where advertising <> '' group by concat(advertising,meaning,name,phone) ORDER BY createDate DESC";
     $res = mysql_query($sql,$con);
 
     while($rs = mysql_fetch_assoc($res))
